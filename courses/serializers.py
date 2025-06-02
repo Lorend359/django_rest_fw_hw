fd555c22ed963b53e7c863860b75c7c0505848b0
Svyatoslav <arend359@gmail.com>
2025-06-02 22:08:58 +0300

    Add nested lesson list to CourseSerializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -2,8 +2,15 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
+class LessonShortSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ('id', 'title', 'description', 'video_url')
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
+    lessons = LessonShortSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
