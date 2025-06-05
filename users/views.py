79eec4b8d9a7b9de0e76b6d9212062f06af19509
Svyatoslav <arend359@gmail.com>
2025-06-05 21:45:37 +0300

    Add JWT auth via email

--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
-from rest_framework import generics
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, serializers
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from .models import CustomUser, Payment
 from .serializers import UserSerializer, PaymentSerializer
@@ -9,17 +13,42 @@ from .serializers import UserSerializer, PaymentSerializer
 class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class UserCreateAPIView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
 
 class PaymentListAPIView(generics.ListAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['course', 'lesson', 'payment_method']
     ordering_fields = ['payment_date']
     ordering = ['-payment_date']  # новые сверху
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        return token
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user = CustomUser.objects.filter(email=email).first()
+        if user and user.check_password(password):
+            return super().validate(attrs)
+
+        raise serializers.ValidationError("Invalid credentials")
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
